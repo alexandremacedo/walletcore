d49f63104e2937387c2b23a2bcb1e4102a87d209
alexandre.macedo <alexandre.macedo@solucx.com.br>
2024-02-07 07:27:26 -0300

    fix: uow usage

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 
@@ -12,6 +13,7 @@ import (
 	"github.com.br/alexandremacedo/walletcore/internal/web"
 	webserver "github.com.br/alexandremacedo/walletcore/internal/web/webserver"
 	"github.com.br/alexandremacedo/walletcore/pkg/events"
+	"github.com.br/alexandremacedo/walletcore/pkg/uow"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -28,11 +30,21 @@ func main() {
 
 	clientDb := database.NewClientDB(db)
 	accountDb := database.NewAccountDB(db)
-	transactionDb := database.NewTransactionDB(db)
+
+	ctx := context.Background()
+	uow := uow.NewUow(ctx, db)
+
+	uow.Register("AccountDB", func(tx *sql.Tx) interface{} {
+		return database.NewAccountDB(db)
+	})
+
+	uow.Register("TransactionDB", func(tx *sql.Tx) interface{} {
+		return database.NewTransactionDB(db)
+	})
 
 	createClientUseCase := create_client.NewCreateClientUseCase(clientDb)
 	createAccountUseCase := create_account.NewCreateAccountUseCase(accountDb, clientDb)
-	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(transactionDb, accountDb, eventDispatcher, transactionCreatedEvent)
+	createTransactionUseCase := create_transaction.NewCreateTransactionUseCase(uow, eventDispatcher, transactionCreatedEvent)
 
 	webserver := webserver.NewWebServer(":3000")
 
